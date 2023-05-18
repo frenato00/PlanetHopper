321bc77de8993b860e6178dd46af043f4fe5c484
frenato00 <up201908044@up.pt>
2023-05-18 14:49:33 +0100

    fix camera-player deviation

--- PlanetHopper/Assets/Scripts/PlayerCamera.cs
+++ PlanetHopper/Assets/Scripts/PlayerCamera.cs
@@ -28,12 +28,16 @@ public class PlayerCamera : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         // transform.forward = orientation.forward;
-        Quaternion targetRotation = Quaternion.FromToRotation(transform.up,orientation.up)* transform.rotation*Quaternion.Euler(xRotation, mouseX, 0);
+        // Quaternion targetRotation = Quaternion.FromToRotation(transform.up,orientation.up)* transform.rotation*Quaternion.Euler(xRotation, mouseX, 0);
+        Quaternion targetRotation = orientation.rotation*Quaternion.Euler(xRotation, mouseX, 0);
         transform.rotation = targetRotation;
         // transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,25*Time.deltaTime);
         // transform.rotation = Quaternion.FromToRotation(transform.up, orientation.up);
         orientation.rotation *= Quaternion.Euler(0, mouseX, 0);
-        // orientation.rotation = targetRotation;
+        // Vector3 forward = Vector3.Dot(transform.forward,orientation.forward)*orientation.forward;
+        // Vector3 right = Vector3.Dot(transform.right,orientation.right)*orientation.right;
+        // orientation.forward=forward;
+        // orientation.right=right;
 
     }
 }
