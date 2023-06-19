8b9e08d9850b94ded7ff7deb64b0c8934579f584
DFMM11 <diomaia654@hotmail.com>
2023-06-19 02:31:26 +0100

    Added all sounds for now

--- PlanetHopper/Assets/Scripts/PlayerShoot.cs
+++ PlanetHopper/Assets/Scripts/PlayerShoot.cs
@@ -7,9 +7,12 @@ public class PlayerShoot : MonoBehaviour
 {
     public static Action shootInput;
 
+    
+
     private void Update(){
         if(GameManager.instance.IsAcceptingPlayerInput() && Input.GetMouseButton(0)){
             shootInput?.Invoke();
+
         }
     }
 }
--- PlanetHopper/Assets/Scripts/Target.cs
+++ PlanetHopper/Assets/Scripts/Target.cs
@@ -6,6 +6,8 @@ public class Target : MonoBehaviour, IDamageable
 {
     public float maxHealth = 50f;
 
+    public FMODUnity.EventReference hitSFX;
+
     private float health;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,9 @@ public class Target : MonoBehaviour, IDamageable
 
     public void TakeDamage(float damage){
         health -= damage;
+
+        FMODUnity.RuntimeManager.PlayOneShot(hitSFX, transform.position);
+
         if(health <= 0){
             
             //disable the object
